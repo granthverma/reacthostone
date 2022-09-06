112d2e8f87fed5ee9c5b44c1df1497819ed98af1
GRANTH <granth@GRANTHs-MacBook-Pro.local>
2022-09-06 22:36:17 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,16 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         host my react app to pubilc
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
       </header>
     </div>
   );
